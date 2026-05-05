6326053e3b702bf297bd916734e6bb5be468ed04
ShirelyM <41966104+ShirelyM@users.noreply.github.com>
2026-05-04 22:43:03 -0400

    Administrative add of updated requirements and version files

---
+++ nexus_workspace/_version.py
@@ -0,0 +1,14 @@
+# ============================================================================
+#
+# Copyright (c) 2026 Reel Big Buoy Company
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# ...
+#
+# Nexus Core
+# File: _version.py
+# Description: Central package version declaration.
+#
+# ============================================================================
+
+__version__ = "0.1.0"
